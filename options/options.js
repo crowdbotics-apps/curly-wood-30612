dbd8e4c81de2c7797c0cbeeadc11de52323d285d
Crowdbotics <team@crowdbotics.com>
2021-09-16 18:11:28 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "curly_wood_30612Identifier",
+  url: "https://curly_wood_30612Identifier.botics.co",
+  api: "https://curly_wood_30612Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
